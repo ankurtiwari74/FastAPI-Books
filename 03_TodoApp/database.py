5c4e375ac9d0d45a791826dea997502d1008ffdb
Ankur Tiwari <ankurtiwari74@yahoo.com>
2024-12-31 13:51:27 +0530

    Project 3.5 Code

--- 03_TodoApp/database.py
+++ 03_TodoApp/database.py
@@ -2,9 +2,17 @@ from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./todos.db"
+# sqlite3 Configuration
+# SQLALCHEMY_DATABASE_URL = "sqlite:///./todosapp.db"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False})
+# Postgresql Configuration
+# SQLALCHEMY_DATABASE_URL = "postgresql://postgres:test1234!@localhost:5432/TodoApplicationDatabase"
+# engine = create_engine(SQLALCHEMY_DATABASE_URL)
+
+# MySQL Configuration
+SQLALCHEMY_DATABASE_URL = "mysql+pymysql://root:test1234!@127.0.0.1:3306/TodoApplicationDatabase"
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
