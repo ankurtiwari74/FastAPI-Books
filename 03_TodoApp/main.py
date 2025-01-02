03ac54d65621412f4d6f7f23b7d6fcca5fb52049
Ankur Tiwari <ankurtiwari74@yahoo.com>
2025-01-02 23:19:27 +0530

    Project-3 : Creating the data

--- 03_TodoApp/main.py
+++ 03_TodoApp/main.py
@@ -1,13 +1,18 @@
 from fastapi import FastAPI
 from database import engine
 from routers import auth, todos, admin, users
-
-import models
+from models import Base
 
 
 app = FastAPI()
 
-models.Base.metadata.create_all(bind=engine)
+Base.metadata.create_all(bind=engine)
+
+
+@app.get("/healthy")
+def health_check():
+    return {"status": "Healthy"}
+
 
 app.include_router(auth.router)
 app.include_router(todos.router)
--- 03_TodoApp/routers/users.py
+++ 03_TodoApp/routers/users.py
@@ -36,7 +36,7 @@ class UserVerification(BaseModel):
 
 @router.get("/", status_code=status.HTTP_200_OK)
 async def get_current_user(user: user_dependency,
-                            db: db_dependency):
+                           db: db_dependency):
     if user is None:
         raise HTTPException(status_code=401, detail="Authentication Failed")
     return db.query(Users).filter(Users.id == user.get("id")).first()
